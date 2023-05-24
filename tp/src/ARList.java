6d3092df544bcc19b633df15ff2a3619f2bcbcd3
Ivan Charabora <ivan@koibanx.com>
2023-05-24 18:23:52 -0300

    comentando el tail, quizas hay que usarlo para algo

--- tp/src/ARList.java
+++ tp/src/ARList.java
@@ -2,11 +2,11 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class ARList<T> {
     public AtomicMarkableReference<ARNode<T>> head;
-    public AtomicMarkableReference<ARNode<T>> tail;
+    //public AtomicMarkableReference<ARNode<T>> tail;
 
     public ARList() {
         ARNode<T> centinell = new ARNode<T>(null);
         head = new AtomicMarkableReference<ARNode<T>>(centinell, false);
-        tail = new AtomicMarkableReference<ARNode<T>>(centinell, false);
+        //tail = new AtomicMarkableReference<ARNode<T>>(centinell, false);
     }
 }
