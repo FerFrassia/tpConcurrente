b93ac7e4a726cf1f87af4659bc544ba269dcdd7f
Ivan Charabora <ivan@koibanx.com>
2023-05-28 13:37:14 -0300

    cambiar tests asi se puede cambiar facil el numero de elementos

--- tp/src/Tester.java
+++ tp/src/Tester.java
@@ -3,6 +3,7 @@ import java.util.concurrent.CountDownLatch;
 public class Tester {
 
     boolean print_tiempo_tests = true;
+    int cantidad_elementos = 10000;
 
     public void correrTests() {
         System.out.println("Corriendo Tests:");
@@ -26,20 +27,20 @@ public class Tester {
     }
 
     public void correrTestsParaConjunto(Conjunto<Integer> c, String mensajeDeSalida) {
-        unHiloAgregando1000Numeros(c, mensajeDeSalida);
-        unHiloSacando1000Numeros(c, mensajeDeSalida);
-        dosHilosAgregando1000Numeros(c, mensajeDeSalida);
-        dosHilosQuitando1000Numeros(c, mensajeDeSalida);
-        cuatroHilosAgregando1000Numeros(c, mensajeDeSalida);
-        cuatroHilosQuitando1000Numeros(c, mensajeDeSalida);
+        unHiloAgregandoCantidadElementosNumeros(c, mensajeDeSalida);
+        unHiloSacandoCantidadElementosNumeros(c, mensajeDeSalida);
+        dosHilosAgregandoCantidadElementosNumeros(c, mensajeDeSalida);
+        dosHilosQuitandoCantidadElementosNumeros(c, mensajeDeSalida);
+        cuatroHilosAgregandoCantidadElementosNumeros(c, mensajeDeSalida);
+        cuatroHilosQuitandoCantidadElementosNumeros(c, mensajeDeSalida);
         testearHilosAgregandoYQuitandoCuatroYSeis(c, mensajeDeSalida);
     }
 
-    public void unHiloAgregando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "unHiloAgregando1000Numeros: ");
+    public void unHiloAgregandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "unHiloAgregando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
-        Integer[] intArray = new Integer[1000];
-        for (Integer i = 0; i < 1000; i = i + 1) {
+        Integer[] intArray = new Integer[cantidad_elementos];
+        for (Integer i = 0; i < cantidad_elementos; i = i + 1) {
             c.agregar(i);
             intArray[i] = i;
         }
@@ -48,17 +49,17 @@ public class Tester {
         c.vaciar();
     }
 
-    public void unHiloSacando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "unHiloSacando1000Numeros: ");
+    public void unHiloSacandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "unHiloSacando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
-        Integer[] intArray = new Integer[1000];
-        for (Integer i = 0; i < 1000; i = i + 1) {
+        Integer[] intArray = new Integer[cantidad_elementos];
+        for (Integer i = 0; i < cantidad_elementos; i = i + 1) {
             c.agregar(i);
             intArray[i] = i;
         }
 
         if (c.esEquivalenteA(intArray)) {
-            for (Integer i = 0; i < 1000; i = i + 1) {
+            for (Integer i = 0; i < cantidad_elementos; i = i + 1) {
                 c.quitar(i);
             }
 
@@ -71,19 +72,19 @@ public class Tester {
         c.vaciar();
     }
 
-    //Agregamos 1000 números y creamos un hilo que agrega los pares y otro que agrega los impares
-    public void dosHilosAgregando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "dosHilosAgregando1000Numeros: ");
+    //Agregamos cantidad_elementos números y creamos un hilo que agrega los pares y otro que agrega los impares
+    public void dosHilosAgregandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "dosHilosAgregando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
         int latchGroupCount = 2;
         CountDownLatch latch = new CountDownLatch(latchGroupCount);
-        Thread t1 = new Thread(new HiloAgregadorDePares(c, 0, 999, latch), "T1");
-        Thread t2 = new Thread(new HiloAgregadorDeImpares(c, 1, 999, latch), "T2");
+        Thread t1 = new Thread(new HiloAgregadorDePares(c, 0, cantidad_elementos-1, latch), "T1");
+        Thread t2 = new Thread(new HiloAgregadorDeImpares(c, 1, cantidad_elementos-1, latch), "T2");
 
         t1.start();
         t2.start();
 
-        Integer[] intArray = new Integer[1000];
+        Integer[] intArray = new Integer[cantidad_elementos];
         for(Integer i=0; i < intArray.length; i++) {
             intArray[i] = i;
         }
@@ -92,23 +93,23 @@ public class Tester {
 
     }
 
-    //Agregamos 1000 números y creamos dos hilos que agrega los pares y dos que agregan los impares:
-    public void cuatroHilosAgregando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "cuatroHilosAgregando1000Numeros: ");
+    //Agregamos cantidad_elementos números y creamos dos hilos que agrega los pares y dos que agregan los impares:
+    public void cuatroHilosAgregandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "cuatroHilosAgregando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
         int latchGroupCount = 4;
         CountDownLatch latch = new CountDownLatch(latchGroupCount);
-        Thread t1 = new Thread(new HiloAgregadorDePares(c, 0, 499, latch), "T1");
-        Thread t2 = new Thread(new HiloAgregadorDeImpares(c, 1, 499, latch), "T2");
-        Thread t3 = new Thread(new HiloAgregadorDePares(c, 500, 999, latch), "T3");
-        Thread t4 = new Thread(new HiloAgregadorDeImpares(c, 501, 999, latch), "T4");
+        Thread t1 = new Thread(new HiloAgregadorDePares(c, 0, (cantidad_elementos/2-1), latch), "T1");
+        Thread t2 = new Thread(new HiloAgregadorDeImpares(c, 1, (cantidad_elementos/2-1), latch), "T2");
+        Thread t3 = new Thread(new HiloAgregadorDePares(c, (cantidad_elementos/2), (cantidad_elementos-1), latch), "T3");
+        Thread t4 = new Thread(new HiloAgregadorDeImpares(c, (cantidad_elementos/2+1), (cantidad_elementos-1), latch), "T4");
 
         t1.start();
         t2.start();
         t3.start();
         t4.start();
 
-        Integer[] intArray = new Integer[1000];
+        Integer[] intArray = new Integer[cantidad_elementos];
         for(Integer i=0; i < intArray.length; i++) {
             intArray[i] = i;
         }
@@ -117,14 +118,14 @@ public class Tester {
         c.vaciar();
     }
 
-    //En un conjunto con 1000 números creamos un hilo que quita los pares y otro que quita los impares
-    public void dosHilosQuitando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "dosHilosQuitando1000Numeros: ");
+    //En un conjunto con cantidad_elementos números creamos un hilo que quita los pares y otro que quita los impares
+    public void dosHilosQuitandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "dosHilosQuitando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
         int latchGroupCount = 2;
         CountDownLatch latch = new CountDownLatch(latchGroupCount);
-        Thread t1 = new Thread(new HiloQuitadorDePares(c, 0, 999, latch), "T1");
-        Thread t2 = new Thread(new HiloQuitadorDeImpares(c, 1, 999, latch), "T2");
+        Thread t1 = new Thread(new HiloQuitadorDePares(c, 0, cantidad_elementos-1, latch), "T1");
+        Thread t2 = new Thread(new HiloQuitadorDeImpares(c, 1, cantidad_elementos-1, latch), "T2");
 
         t1.start();
         t2.start();
@@ -134,16 +135,16 @@ public class Tester {
         print_tiempo_tardado(startTime);
     }
 
-    //En un conjunto con 1000 números creamos dos hilos que quitan los pares y otros dos que quitan los impares
-    public void cuatroHilosQuitando1000Numeros(Conjunto<Integer> c, String mensajeDeSalida) {
-        System.out.print(mensajeDeSalida + "cuatroHilosQuitando1000Numeros: ");
+    //En un conjunto con cantidad_elementos números creamos dos hilos que quitan los pares y otros dos que quitan los impares
+    public void cuatroHilosQuitandoCantidadElementosNumeros(Conjunto<Integer> c, String mensajeDeSalida) {
+        System.out.print(mensajeDeSalida + "cuatroHilosQuitando" + cantidad_elementos  +"Numeros: ");
         long startTime = System.currentTimeMillis();
         int latchGroupCount = 4;
         CountDownLatch latch = new CountDownLatch(latchGroupCount);
-        Thread t1 = new Thread(new HiloQuitadorDePares(c, 0, 499, latch), "T1");
-        Thread t2 = new Thread(new HiloQuitadorDeImpares(c, 1, 499, latch), "T2");
-        Thread t3 = new Thread(new HiloQuitadorDePares(c, 500, 999, latch), "T3");
-        Thread t4 = new Thread(new HiloQuitadorDeImpares(c, 501, 999, latch), "T4");
+        Thread t1 = new Thread(new HiloQuitadorDePares(c, 0, (cantidad_elementos/2-1), latch), "T1");
+        Thread t2 = new Thread(new HiloQuitadorDeImpares(c, 1, (cantidad_elementos/2-1), latch), "T2");
+        Thread t3 = new Thread(new HiloQuitadorDePares(c, (cantidad_elementos/2), (cantidad_elementos-1), latch), "T3");
+        Thread t4 = new Thread(new HiloQuitadorDeImpares(c, (cantidad_elementos/2+1), (cantidad_elementos-1), latch), "T4");
 
         t1.start();
         t2.start();
@@ -173,16 +174,16 @@ public class Tester {
         }
     }
 
-    //Hilos que agregan y quitan 4 y 6 mil veces.
+    //Hilos que agregan y quitan 4 y 6 muchas veces.
     public void hilosAgregandoYQuitandoCuatroYSeis(Conjunto<Integer> c, String mensajeDeSalida) {
         System.out.print(mensajeDeSalida + "hilosAgregandoYQuitandoCuatroYSeis: ");
 
         int latchGroupCount = 4;
         CountDownLatch latch = new CountDownLatch(latchGroupCount);
-        Thread t1 = new Thread(new HiloAgregadorIterativamente(c, 4, 1000, latch), "T1");
-        Thread t2 = new Thread(new HiloAgregadorIterativamente(c, 6, 1000, latch), "T2");
-        Thread t3 = new Thread(new HiloQuitadorIterativamente(c, 4, 1000, latch), "T3");
-        Thread t4 = new Thread(new HiloQuitadorIterativamente(c, 6, 1000, latch), "T4");
+        Thread t1 = new Thread(new HiloAgregadorIterativamente(c, 4, cantidad_elementos, latch), "T1");
+        Thread t2 = new Thread(new HiloAgregadorIterativamente(c, 6, cantidad_elementos, latch), "T2");
+        Thread t3 = new Thread(new HiloQuitadorIterativamente(c, 4, cantidad_elementos, latch), "T3");
+        Thread t4 = new Thread(new HiloQuitadorIterativamente(c, 6, cantidad_elementos, latch), "T4");
 
         t1.start();
         t2.start();
