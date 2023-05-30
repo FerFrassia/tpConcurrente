dee5a7abfe30ae016fa9c125b9a32527054a7df2
Fernando N. Frassia <ferfrassia@gmail.com>
2023-05-30 16:26:04 -0300

    Descomento tester

--- tp/src/Main.java
+++ tp/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       //new Tester().correrTests();
+       new Tester().correrTests();
        new Experimenter().correrExperimentos();
     }
 }
